571c091861ea861836b631de3d2900307cc45379
Joel Salisbury <joel.saisbury@gmail.com>
2023-05-01 14:38:58 -0400

    new tweaks

--- app/models/Checkout.php
+++ app/models/Checkout.php
@@ -1,5 +1,4 @@
 <?php
-namespace App\Models;
 class Checkout {
     public $id;
     public $user_id;
--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,4 @@
 <?php
-namespace App\Models;
 class User {
     private $id;
     private $name;
